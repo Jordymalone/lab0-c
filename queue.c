3319d8ef3d4207343ee53ec30e738cd87e0db4dc
Jordan Chiu <jordan871130@gmail.com>
2025-03-04 10:09:13 +0800

    Feat: Implement q_remove_head and q_remove_tail
    This commit introduces functions to remove an element from the head or
    tail of a queue without deleting its memory. Removal here means
    unlink the node from the linked list, not freeing it.
    
    The functions use list_first_entry or list_last_entry to locate the
    node, then call a helper to remove the node from the list and reset its
    pointers. If a buffer is provided, strncpy is used to copy the node's
    string before returning the node.
    
    Change-Id: I52ab64ef29d0229a38152d4ddbc3470d5e18df40

--- queue.c
+++ queue.c
@@ -78,13 +78,31 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+    element_t *remove_elem = list_first_entry(head, element_t, list);
+    list_del_init(&remove_elem->list);
+    if (sp) {
+        strncpy(sp, remove_elem->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return remove_elem;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+    element_t *remove_elem = list_last_entry(head, element_t, list);
+    list_del_init(&remove_elem->list);
+    if (sp) {
+        strncpy(sp, remove_elem->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return remove_elem;
 }
 
 /* Return number of elements in queue */
